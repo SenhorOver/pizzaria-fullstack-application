fcf95e06dbc1b8a975e630432ab0d8a22edff72b
Marcos V Silva <mrcsvs@outlook.com>
2025-08-09 18:18:11 -0300

    POST Add order item

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -16,6 +16,7 @@ import uploadConfig from "./config/multer";
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+import { AddItemController } from "./controllers/order/AddItemController";
 
 const router = Router();
 
@@ -55,5 +56,7 @@ router.get(
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
+
+router.post("/order/add", isAuthenticated, new AddItemController().handle);
 
 export { router };
