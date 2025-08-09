d9f0fd771feb222a1791cd913d1d20c453d0206c
Marcos V Silva <mrcsvs@outlook.com>
2025-08-08 21:12:30 -0300

    POST Create category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
@@ -15,5 +16,10 @@ router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 // -- ROUTES CATEGORY
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle,
+);
 
 export { router };
