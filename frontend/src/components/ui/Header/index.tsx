271ac0bbcb19f0b0ed9b552f9344c296513fb79b
Marcos V Silva <mrcsvs@outlook.com>
2025-08-22 19:03:55 -0300

    Header Component + SignOut

---
+++ frontend/src/components/ui/Header/index.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import styles from "./style.module.scss";
+import Image from "next/image";
+import { FiLogOut } from "react-icons/fi";
+import { AuthContext } from "@/contexts/AuthContext";
+import { useContext } from "react";
+
+export function Header() {
+  const { signOut } = useContext(AuthContext);
+
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href={"/dashboard"}>
+          <Image
+            src={"/favicon.svg"}
+            width={250}
+            height={75}
+            alt="Logo do site Sujeiro Pizzaria"
+          />
+        </Link>
+        <nav className={styles.menuNav}>
+          <Link href={"/category"}>
+            <p>Categoria</p>
+          </Link>
+
+          <Link href={"/product"}>
+            <p>Cardapio</p>
+          </Link>
+
+          <button onClick={signOut}>
+            <FiLogOut color="#FFF" size={24} />
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
+}
