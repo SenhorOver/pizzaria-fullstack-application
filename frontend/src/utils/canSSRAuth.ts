0461eb5b4bbc678e9ce910966c14169b6558cc33
Marcos V Silva <mrcsvs@outlook.com>
2025-08-21 22:34:30 -0300

    Protect Authenticated pages

---
+++ frontend/src/utils/canSSRAuth.ts
@@ -0,0 +1,50 @@
+import { AuthTokenError } from "@/services/errors/AuthTokenError";
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies, destroyCookie } from "nookies";
+
+// Only Authenticated
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function canSSRAuth<P extends Record<string, any>>(
+  fn: GetServerSideProps<P>,
+) {
+  return async (
+    ctx: GetServerSidePropsContext,
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+    // If guest user access page, redirect
+    const token = cookies["@nextauth.token"];
+    if (!token) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+
+    try {
+      return await fn(ctx);
+    } catch (err) {
+      if (err instanceof AuthTokenError) {
+        destroyCookie(ctx, "@nextauth.token");
+        return {
+          redirect: {
+            destination: "/",
+            permanent: false,
+          },
+        };
+      }
+      destroyCookie(ctx, "@nextauth.token");
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+  };
+}
